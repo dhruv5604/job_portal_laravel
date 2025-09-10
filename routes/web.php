007905392d6318065d64a0470ec8f00ba54692d1
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-09-10 11:27:32 +0530

    change in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,18 @@
 use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('front.home');
-})->name('home');
+Route::view('/', 'front.home')->name('home');
 
+// Guest user routes
 Route::middleware('guest')->prefix('account')->name('account.')->group(function () {
-    Route::get('/register', function () {
-        return view('front.account.registration');
-    })->name('register');
+    Route::view('/register','front.account.registration')->name('register');
     Route::post('/process-register', [AccountController::class, 'processRegistration'])->name('processRegistration');
-    Route::get('/login', function () {
-        return view('front.account.login');
-    })->name('login');
+    Route::view('/login','front.account.login')->name('login');
     Route::post('/authenticate', [AccountController::class, 'authenticate'])->name('authenticate');
 });
 
 // Authenticated user routes
 Route::middleware('auth')->prefix('account')->name('account.')->group(function () {
-    Route::get('/profile', function () {
-        return view('front.account.profile');
-    })->name('profile');
+    Route::view('/profile', 'front.account.profile')->name('profile');
     Route::get('/logout', [AccountController::class, 'logout'])->name('logout');
 });
