ec7b03cbccdccd3a4e8d42ff42601a2e9310e74e
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-09-22 18:27:07 +0530

    showing featured job in random order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,10 @@ use App\Models\Job;
 
 class HomeController extends Controller
 {
-    //
     public function index()
     {
         $categories = Category::where('status', 1)->take(8)->get();
-        $featuredJobs = Job::where('isFeatured', 1)->where('status', 1)->with('jobType')->take(6)->get();
+        $featuredJobs = Job::where('isFeatured', 1)->where('status', 1)->inRandomOrder()->with('jobType')->take(6)->get();
         $latestJobs = Job::where('status', 1)->with('jobType')->latest()->take(6)->get();
 
         return view('front.home', compact('categories', 'featuredJobs', 'latestJobs'));
